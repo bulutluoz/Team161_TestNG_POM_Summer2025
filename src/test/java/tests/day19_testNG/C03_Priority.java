3977e7d2f4f63daf3e6e43b579d47b1e374d4d94
bulutluoz <abbulutluoz@gmail.com>
2025-05-30 20:12:50 +0200

    day19 sonu

--- src/test/java/tests/day19_testNG/C03_Priority.java
+++ src/test/java/tests/day19_testNG/C03_Priority.java
@@ -21,6 +21,12 @@ public class C03_Priority {
 
         Kurallar :
         1- priority degeri kucuk olandan buyuge dogru calisir
+        2- eger bir test method'una priority degeri atanmazsa
+           TestNG default olarak o method'a priority degeri olarak 0 verir
+        3- eger birden fazla method'a ayni priority degeri atanirsa
+           priority deger ayni olanlar
+           kendi iclerinde isim sirasina gore calisir
+
      */
 
     // 3 farkli test method'u olusturarak, asagidaki gorevleri yapin
@@ -42,7 +48,7 @@ public class C03_Priority {
         driver.quit();
     }
 
-    @Test
+    @Test // priority = 0
     public void anasayfaTesti(){
         // 1. Testotomasyonu anasayfaya gidip, url'in testotomasyonu icerdigini test edin
 
@@ -54,8 +60,7 @@ public class C03_Priority {
         Assert.assertTrue(actualUrl.contains(expectedUrlIcerik));
     }
 
-
-    @Test
+    @Test // priority = 0
     public void phoneAramaTesti(){
         // 2. phone icin arama yapip, arama sonucunda urun bulunabildigini test edin
 
@@ -72,8 +77,7 @@ public class C03_Priority {
         Assert.assertNotEquals(actualSonucYazisi,unexpectedSonucYazisi);
     }
 
-
-    @Test
+    @Test(priority = 20)
     public void ilkUrunIsimTesti(){
         // 3. ilk urunu tiklayip,
         driver.findElement(By.xpath("(//*[@class='prod-img'])[1]"))
