b9ade13160cd53f1dc0195bfc0d7266c7b6b48fa
bulutluoz <abbulutluoz@gmail.com>
2025-06-03 19:58:12 +0200

    day21 sonu

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,6 +2,9 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -14,8 +17,27 @@ public class Driver {
 
     public static WebDriver  getDriver(){
 
+        String kullanilacakBrowser = ConfigReader.getProperty("browser");
+        // configuration properties dosyasindan browser tercihini aldik
+
+
         if (driver == null){
-            driver = new ChromeDriver();
+
+            switch (kullanilacakBrowser){
+
+                case "edge" :
+                    driver = new EdgeDriver();
+                    break;
+                case "firefox" :
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari" :
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+            }
+
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.manage().window().maximize();
         }
